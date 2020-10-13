0c913840a8821e57854aee1f93004a017a57e273
David Lentz <davidmlentz@gmail.com>
2020-10-13 09:35:09 -0600

    Add convenience script

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl apply -f datadog-eks-rbac.yaml
+kubectl apply -f nginx-with-datadog-agent-and-logs-and-traces.yaml 
